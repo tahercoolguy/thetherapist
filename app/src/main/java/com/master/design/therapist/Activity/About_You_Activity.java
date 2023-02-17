aa8796df46e6581961394d25f083e33674090b31
Chirag Soni <chirag22soni04@gmail.com>
2023-02-17 11:42:46 +0530

    16-02-2023 about you activity  & all button text size normal

--- app/src/main/java/com/master/design/therapist/Activity/About_You_Activity.java
+++ app/src/main/java/com/master/design/therapist/Activity/About_You_Activity.java
@@ -1,34 +1,43 @@
 package com.master.design.therapist.Activity;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.master.design.therapist.R;
 
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 
-public class Create_Account_Activity extends AppCompatActivity {
+public class About_You_Activity extends AppCompatActivity {
+
+    private Activity activity;
+    private Context context;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_create_account);
+        setContentView(R.layout.activity_about_you);
         ButterKnife.bind(this);
+        context = this.getApplicationContext();
+        activity = this;
     }
 
-    @OnClick(R.id.continueTxt)
-    public void clickcontinueTxt() {
-        startActivity(new Intent(Create_Account_Activity.this, About_You_Activity.class));
-        overridePendingTransition(R.anim.left_slide_in, R.anim.right_slide_out);
-    }
 
     @OnClick(R.id.backImg)
     public void clickBack() {
         finish();
     }
+
+    @OnClick(R.id.signUpTxt)
+    public void clicksignUpTxt() {
+        startActivity(new Intent(About_You_Activity.this, Sign_InActivity.class));
+        overridePendingTransition(R.anim.left_slide_in, R.anim.right_slide_out);
+    }
+
 
     @Override
     public void onBackPressed() {
